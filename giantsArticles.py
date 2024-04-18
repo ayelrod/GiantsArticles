255606d3f16d9d7664f97047777e72bc18e0d586
Lucas Rodriguez <lucas25rodriguez@gmail.com>
2024-04-17 18:27:14 -0700

    Add support for more websites

--- giantsArticles.py
+++ giantsArticles.py
@@ -6,6 +6,9 @@ from lxml import etree
 
 API_KEYS_FILE = "api_keys.txt"
 DB_FILE = "history.txt"
+HEADER = {
+    'referer':'https://www.google.com/'
+}
 
 class Article:
 	title: str
@@ -28,21 +31,25 @@ class Website:
 
 sites = [
 	Website("The Athletic", "https://theathletic.com/team/sf-giants", '//*[@id="body-container"]/div[3]/div/div/div[1]/div/div/a/div/span/div[2]/div[1]/div/h4/span', '//*[@id="body-container"]/div[3]/div/div/div[1]/div/div/a'),
-	Website("SF Chronicle", "https://www.sfchronicle.com/sports/giants", '', ''),
-	Website("NBCS Bay Area", "https://www.nbcsports.com/bayarea/giants", '', ''),
-	Website("KNBR", "https://www.knbr.com/giantsnews", '', ''),
-	Website("Around The Foghorn", "https://aroundthefoghorn.com", '', ''),
+	# Website("SF Chronicle", "https://www.sfchronicle.com/sports/giants", '//*[@id="__next"]/main/div[4]/div/div/div/div[1]/div/div[2]/h2/a', '//*[@id="__next"]/main/div[4]/div/div/div/div[1]/div/div[2]/h2/a'), # CAPTCHA Protected
+	Website("NBCS Bay Area", "https://www.nbcsports.com/bayarea/giants", '//*[@id="main"]/div[2]/div[2]/div[1]/div[2]/div[2]/a', '//*[@id="main"]/div[2]/div[2]/div[1]/div[2]/div[2]/a'),
+	# Website("KNBR", "https://www.knbr.com/giantsnews", '//*[@id="wp--skip-link--target"]/div[1]/div[1]/ul/li[1]/div/div[2]/h2/a', '//*[@id="wp--skip-link--target"]/div[1]/div[1]/ul/li[1]/div/div[2]/h2/a'), # Not Working
+	Website("Around The Foghorn", "https://aroundthefoghorn.com", '//*[@id="mm-root"]/main/div[2]/section[1]/div/article/div/a', '//*[@id="mm-root"]/main/div[2]/section[1]/div/article/div/a'),
+	Website("The Mercury News", "https://www.mercurynews.com/sports/mlb/san-francisco-giants/", '//*[@id="main"]/section/article/div/header/h3/a/span', '//*[@id="main"]/section/article/figure/div/a')
 ]
 
 def get_latest_article(website: Website) -> Article:
-	home = requests.get(website.link)
+	home = requests.get(website.link, headers=HEADER)
 	content = BeautifulSoup(home.content, 'html.parser')
 	parsed_content = etree.HTML(str(content))
 	
 	title = ""
 	element = parsed_content.xpath(website.title_xpath)
 	while len(element) > 0:
-		title += element[0].text
+		if element[0].text != None:
+			title += element[0].text
+		elif element[0].get('title') != None:
+			title += element[0].get('title')
 		element = element[0].getchildren()
 
 	link = ""
@@ -57,7 +64,7 @@ def get_latest_article(website: Website) -> Article:
 	if title == "" or link == "":
 		return None
 	
-	return Article(title, link)
+	return Article(title.strip(), link.strip())
 
 def already_posted(article: Article, history: dict) -> bool:
 	# TODO
@@ -76,15 +83,15 @@ def get_api():
 	keys = apiKeys.readlines()
 	keys = [x.strip() for x in keys]
 
-	#SET API KEYS
-	auth = tweepy.OAuthHandler(keys[0], keys[1])
+	# Create client
+	client = tweepy.Client(
+    		consumer_key=keys[0],
+    		consumer_secret=keys[1],
+    		access_token=keys[2],
+    		access_token_secret=keys[3]
+		)
 
-	#SET ACCESS TOKENS
-	auth.set_access_token(keys[2], keys[3])
-
-	#CREATE API INSTANCE
-	api = tweepy.API(auth)
-	return api
+	return client
 
 def post(article, api):
 	# TODO
